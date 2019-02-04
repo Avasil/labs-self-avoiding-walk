844573452d9fc788668a9b8c00d2c026a7a0ae8d
Avasil <pgawrys2@gmail.com>
2019-02-04 22:33:05 +0100

    remove pseudo auto search

--- src/main/scala/labs/Main.scala
+++ src/main/scala/labs/Main.scala
@@ -9,12 +9,12 @@ object Main extends IOApp {
       params <- IO.fromEither(Parameters.fromArgs(args))
       _ <- putStrLn(s"Running simulation with following parameters: $params")
       _ <- runMany(params, 1)
-      bestParams51 <- findBest(params.copy(length = 51), List(10000, 15000, 20000, 25000, 30000, 35000, 40000))
-      _ <- runMany(bestParams51, 10)
-      bestParams71 <- findBest(params.copy(length = 71), List(10000, 15000, 20000, 25000, 27500, 30000))
-      _ <- runMany(bestParams71, 10)
-      bestParams99 <- findBest(params.copy(length = 99), List(10000, 15000, 20000, 25000, 27500, 30000))
-      _ <- runMany(bestParams99, 10)
+//      bestParams51 <- findBest(params.copy(length = 51), List(10000, 15000, 20000, 25000, 30000, 35000, 40000))
+//      _ <- runMany(bestParams51, 10)
+//      bestParams71 <- findBest(params.copy(length = 71), List(10000, 15000, 20000, 25000, 27500, 30000))
+//      _ <- runMany(bestParams71, 10)
+//      bestParams99 <- findBest(params.copy(length = 99), List(10000, 15000, 20000, 25000, 27500, 30000))
+//      _ <- runMany(bestParams99, 10)
     } yield ExitCode.Success
   }
 
